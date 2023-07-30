43b97352735b09d4c04f0e4dbf781a7b4e70345c
= <=>
2023-07-30 15:18:52 -0400

    got started with what i was going to do

--- movie/load.js
+++ movie/load.js
@@ -64,7 +64,50 @@ module.exports = function (req, res, url) {
 						/* if i am correct, the opening and closing stuff is pretty much for characters, so we need to find a way to fit the correct emotions into the characters 
 						and also lipsync the text to the characters. */
 						if (e) return res.end(JSON.stringify({error: e}));
-						console.log(f);
+						console.log(f);// writes the preview xml
+						const charIdsXml = {
+							"1": "AVATOR134",
+							"2": "AVATOR135"
+						};
+						const charIds = [];
+						let movieXml = `<film copyable="0" duration="7.5" published="0" pshare="0" isWide="1">
+						<meta>
+						  <title><![CDATA[]]></title>
+						  <tag><![CDATA[]]></tag>
+						  <hiddenTag><![CDATA[]]></hiddenTag>
+						  <desc><![CDATA[]]></desc>
+						  <mver><![CDATA[4]]></mver>
+						  <studio>d7446f28669693f5b7a35d41831e43c3662c48bf</studio>
+						  <thumbnail index="0"/>
+						  <palette>
+							<color value="333333"/>
+							<color value="999999"/>
+							<color value="fbfbfb"/>
+							<color value="669999"/>
+							<color value="55bbaa"/>
+							<color value="7fbb11"/>
+							<color value="15709b"/>
+							<color value="9a5d1"/>
+							<color value="6ab8d6"/>
+							<color value="da2021"/>
+							<color value="fecd31"/>
+							<color value="28c6c1"/>
+							<color value="37444f"/>
+						  </palette>
+						</meta>`;
+						const counts = {
+							chars: 0,
+							scripts: 0
+						}
+						for (const data in f) {
+							if (data.includes(`script`)) {
+								if (data.includes(`script[${counts.chars}][cid]`) && counts.chars < 2) { // pushes char ids to the charIds table for later usage.
+									charIds.push(f[`script[${counts.chars}][cid]`]);
+									counts.chars++
+								}
+							}
+						}
+						console.log(charIds, counts);
 						res.end(JSON.stringify({
 							error: "the golite movie preview system is in beta right now. please check back later."
 						}))
@@ -84,11 +127,10 @@ module.exports = function (req, res, url) {
 					res.setHeader("Content-Type", "application/zip");
 					loadPost(req, res).then(async ([data]) => {
 						try {
-							if (movieId != "templatePreview") {
+							if (url.query.movieId != "templatePreview") {
 								const b = await movie.loadZip(url.query, data);
 								res.end(Buffer.concat([base, b]));
-							} else { // what we should do during a template preview.
-							}
+							} else res.end(Buffer.concat([base, await parse.packMovie(fs.readFileSync("./previews/template.xml"))]));
 						} catch (e) {
 							console.log(e);
 							res.end(1 + e);
