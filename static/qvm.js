6020dd808561e52bfe0a2cdeab5dfd4482279cfb
= <=>
2023-07-31 15:16:01 -0400

    finally put in the ga4s studio

--- static/qvm.js
+++ static/qvm.js
@@ -2,14 +2,7 @@ const https = require("https");
 const fs = require("fs");
 
 module.exports = (req, res, url) => {
-    const pathnames = [
-        "/qvm_files",
-        "/lvm_files"
-    ]
-    for (const pathname of pathnames) {
-        if (req.method != "GET") return;
-        if (url.pathname.startsWith(pathname)) res.end(fs.readFileSync(`.${url.pathname}`));
-        else return;
-        return true;
-    }
+    if (req.method != "GET" || !url.pathname.startsWith("/qvm_files") && !url.pathname.startsWith("/lvm_files")) return;
+    res.end(fs.readFileSync(`.${url.pathname}`));
+    return true;
 }
