abbca1171ea88663fbea774f320bf3d53250f71b
= <=>
2023-08-06 23:21:40 -0400

    fixed a bug where the css would turn into text/plain on some hosting services

--- static/qvm.js
+++ static/qvm.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 module.exports = (req, res, url) => {
     if (req.method != "GET" || !url.pathname.startsWith("/qvm_files") && !url.pathname.startsWith("/lvm_files")) return;
+    if (url.pathname.endsWith(".css")) res.setHeader("Content-Type", "text/css");
     res.end(fs.readFileSync(`.${url.pathname}`));
     return true;
 }
